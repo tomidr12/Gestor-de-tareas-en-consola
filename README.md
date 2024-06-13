6baae586b40a7853ddb0dc3ad41d6686a63cc592
tomidr12 <delriotomas12@gmail.com>
2024-06-13 17:51:32 -0300

    Proyecto terminado

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# gestor-de-tareas-en-consola
+# Gestor de Tareas de EstudioAud
+
+¡Bienvenido al Gestor de Tareas de EstudioAud! 🎉
+
+Este proyecto es un gestor de tareas sencillo pero potente, diseñado para ayudarte a mantenerte organizado y productivo. 📚✨
+
+## Características
+
+- **Añadir Tareas**: Permite añadir nuevas tareas a tu lista.
+- **Ver Tareas**: Muestra todas las tareas pendientes.
+- **Marcar Tareas como Completadas**: Te permite marcar las tareas que ya has completado.
+- **Eliminar Tareas**: Puedes eliminar las tareas que ya no necesites.
+
+## Uso
+
+Sigue los siguientes pasos para utilizar el gestor de tareas:
+
+1. **Añadir Tarea**: Ingresa el número `1` en el menú principal y sigue las instrucciones para añadir una nueva tarea.
+2. **Ver Tareas**: Ingresa el número `2` en el menú principal para ver la lista de tareas actuales.
+3. **Marcar Tarea como Completada**: Ingresa el número `3` y luego el número de la tarea que deseas marcar como completada.
+4. **Eliminar Tarea**: Ingresa el número `4` y luego el número de la tarea que deseas eliminar.
+5. **Salir**: Ingresa el número `5` para salir del programa.
+
+## Instalación
+
+1. Clona el repositorio:
+    ```bash
+    git clone https://github.com/tomidr12/gestor-de-tareas-en-consola.git
+    ```
+2. Navega al directorio del proyecto:
+    ```bash
+    cd gestor-de-tareas
+    ```
+3. Ejecuta el script principal:
+    ```bash
+    python main.py
+    ```
+
+## Contribuciones
+
+¡Las contribuciones son bienvenidas! Si deseas mejorar el gestor de tareas, por favor sigue los siguientes pasos:
+
+1. Haz un fork del repositorio.
+2. Crea una nueva rama (`git checkout -b feature/nueva-caracteristica`).
+3. Realiza tus cambios y haz commit (`git commit -am 'Añadir nueva característica'`).
+4. Sube tus cambios (`git push origin feature/nueva-caracteristica`).
+5. Crea un nuevo Pull Request.
+
+## Contacto
+
+Si tienes alguna pregunta o sugerencia, no dudes en contactarme a través de [LinkedIn](www.linkedin.com/in/tomás-del-rio).
+
+---
+
+¡Gracias por utilizar el Gestor de Tareas de EstudioAud! 🚀
+
+---
--- funciones.py
+++ funciones.py
@@ -4,47 +4,77 @@ tareas = []
 def agregar_tarea(lista):
   # Entrada para la tarea
   tarea = input("\nIngrese la tarea a realizar: ")
-  
   if tarea != "":
     lista.append(tarea)
-    
   # Informa de tarea añadida
     print("\n*******************************************")
     print("***** --- Tarea añadida con exito --- *****")
     print("*******************************************\n")
-    
   # Imprime la tarea añadida
     print("------------------------------------------------")
     print(tarea)
     print("------------------------------------------------")
-
   # Informa del numero de tarea
     print(f"\nNumero de tarea: {len(lista)}")
-  
+    
 def ver_tareas(lista):
-
   # Condicional que evalue si hay algo en la lista
   if lista:
-    
     # Si hay algo en la lista se presenta
     print("\n***********************************")
     print("***** --- Lista de tareas --- *****")
     print("***********************************\n")
-    
     for i in range(len(lista)):
       print(f"{i+1}. {lista[i]}")
-      
   else:
     #Si la lista esta vacia se avisa de ello
     print("\n*********************************")
     print("***** --- No hay tareas --- *****")
-    print("*********************************\n")
-    
+    print("*********************************\n") 
   #Mensaje de fin de listado 
-  print("--- FIN DEL LISTADO DE TAREAS ---")
+  print("\n--- FIN DEL LISTADO DE TAREAS ---")
   
 def marcar_tarea(lista):
-  print(lista)
+  # Llamamos a la funcion ver tareas
+  ver_tareas(lista)
+  # Entrada para que el usuraio introduzca una tarea
+  completada = int(input("\nIntroduzca el numero de la tarea a marcar como completada: "))
+  # Condicional para marcar las tareas completadas
+  if completada > 0 and completada <= len(lista):
+    # Condicional para evaluar si la tarea ya esta completada
+    # Si la tarea ya esta completada
+    if "(Completada)" in lista[completada - 1]:
+      print("La tarea ya esta completada")
+    # Si la tarea no esta completada
+    else:
+      # Marcar la tarea como completada
+      lista[completada - 1] = f"{lista[completada - 1]} (Completada)"
+      print("\n*************************************************")
+      print("***** --- Tarea marcada como completada --- *****")
+      print("*************************************************\n")
+  # Avisar si la opcion elegida es invalida
+  else:
+    print("\nLa opcion elegida no es valida")
 
 def eliminar_tarea(lista):
-  print(lista)
+  # Si la lista contiene algo
+  if lista:
+    # Llamamos a la funcion ver_tareas()
+    ver_tareas(lista)
+    # Entrada para que el usuario introduza una tarea
+    tarea_a_eliminar = int(input("\nIngrese el numero de la tarea que quiere eliminar: "))
+    # Opcion invalida si la atrea no esta en el rango de la lista
+    if tarea_a_eliminar <= 0 or tarea_a_eliminar > len(lista):
+      print("\nLa opcion elegida no es valida")
+    # Si la opcion es valida se elimina la tarea
+    else:
+      # Se elimina la tarea
+      del lista[tarea_a_eliminar - 1]
+      print("\n*********************************************")
+      print("***** --- Tarea eliminada con exito --- *****")
+      print("*********************************************\n")
+  # Si la lista esta vacia se avisa de ello
+  else:
+    print("\n*********************************")
+    print("***** --- No hay tareas --- *****")
+    print("*********************************\n")
